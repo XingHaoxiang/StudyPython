a2c100ee49920525357840786d4dca2f9abc431d
XingHaoxiang <494989582@qq.com>
2017-03-10 23:27:47 +0800

    first commit studyPython

---
+++ hello.py
@@ -0,0 +1,29 @@
+list = [1, 2, 3]
+trupe = ('x', 'y', 'z')
+
+for x in trupe:
+    print x
+
+# -------------
+print abs(-100)
+print cmp(1, 1)
+a = abs
+print 'a=abs', a(-1)
+
+
+def my_abs(x):
+    if x >= 0:
+        return x;
+    if x < 0:
+        return -x;
+
+print my_abs(-2)
+
+age = 19
+if age>18:
+    pass
+print my_abs('A')
+
+def my_abs(x):
+    if
+    pass
